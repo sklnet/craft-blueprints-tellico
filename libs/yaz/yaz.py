2e9cdbaa0dcb890e4c66bf3bfa3fb4f7968ad2a4
Andrea <andrea.randazzo@unige.it>
2019-11-16 19:37:27 +0100

    Update yaz

--- libs/yaz/yaz.py
+++ libs/yaz/yaz.py
@@ -3,10 +3,10 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        ver = '5.25'
-        self.targets[ver] = 'yaz-%s-lib-win64.zip' % ver
+        ver = '5.25.0'
+        self.targets[ver] = 'https://github.com/sklnet/craft-blueprints-tellico/raw/master/libs/yaz/yaz-%s-lib-win64.zip' % ver
         self.targetInstSrc[ver] = 'yaz-'+ver
-        self.targetDigests[ver] = 'ad1d92db2eba69f9200c233743f280010cb11e21'
+        self.targetDigests[ver] = '0f00b41e61de47a19ee616baa3e62e7d2c8d649c'
         self.description = 'yaz'
         self.defaultTarget = ver
 
@@ -15,3 +15,15 @@ from Package.BinaryPackageBase import *
 class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
+        self.subinfo.options.package.withCompiler = False
+        self.subinfo.options.package.withSources = False
+
+    def unpack(self):
+        if not BinaryPackageBase.unpack(self): return False
+        return True
+
+    def install(self):
+        if not os.path.isdir(os.path.join(self.installDir(), "bin")):
+            os.makedirs(os.path.join(self.installDir(), "bin"))
+        shutil.copy(os.path.join(self.sourceDir(), "bin", "*"),os.path.join(self.installDir(), "bin"))
+					
